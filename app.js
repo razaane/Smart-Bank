d4e04af3604b779c1cb741e10c81feef060a4cca
razaane <wakhidirazane@gmail.com>
2026-09-08 23:42:18 +0100

    j'ai ajouter la fonction de renderSignUp

--- app.js
+++ app.js
@@ -1,30 +1,53 @@
-// const root = document.getElementById("root");
-// const message = document.createElement("h1");
-// message.textContent ="hey girl";
-// root.appendChild(message);
-
 const root = document.getElementById("root");
 
-function render() {
-  const hash = window.location.hash; 
+function renderSignUp(){
+  root.innerHTML="";
 
-  
-  root.innerHTML = "";
+  const title =document.createElement("h1");
+  title.textContent ="Créer un compte";
 
-  
-  const title = document.createElement("h1");
+  const fullName = document.createElement("input");
+  fullName.type = "text";
+  fullName.placeholder="Enter votre nomet prénom ";
+  fullName.id = "fullname";
 
-  if (hash === "#/dashboard") {
-    title.textContent = "Ana f Dashboard";
-  } else if (hash === "#/offres") {
-    title.textContent = "Ana f Offres";
-  } else {
-    title.textContent = "Ana f Accueil";
-  }
+  const email = document.createElement("input");
+  email.type="email";
+  email.placeholder="Entrer votre adresse email ";
+  email.id ="email";
 
+  const phone = document.createElement("input");
+  phone.type="telephone";
+  phone.placeholder="+212*********";
+  phone.id = "phone";
+
+  const password = document.createElement("input");
+  password.type ="password";
+  password.placeholder="Entrer un mot de passe";
+  password.id="password";
+
+  const passwordVer = document.createElement("input");
+  passwordVer.type ="password";
+  passwordVer.placeholder="Confirmer votre mot de passe";
+  passwordVer.id="password";
+
+  const submitBtn= document.createElement("button");
+  submitBtn.textContent ="S'inscrire";
+
+  submitBtn.addEventListener("click",()=>{
+    const actualEmail = email.value;
+    const actualPass = password.value;
+    console.log("email is :",actualEmail);
+    console.log("password is :",actualPass);
+  });
 
   root.appendChild(title);
+  root.appendChild(fullName);
+  root.appendChild(email);
+  root.appendChild(phone);
+  root.appendChild(password);
+  root.appendChild(passwordVer);
+  root.appendChild(submitBtn);
 }
 
-window.addEventListener("hashchange", render);
-window.addEventListener("DOMContentLoaded", render);
+renderSignUp();
