e7568f7df3e98c764e279378693bf188210ad977
razaane <wakhidirazane@gmail.com>
2026-09-09 00:36:54 +0100

    j'ai ajouter la validaton des données par regex

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function renderSignUp(){
   email.id ="email";
 
   const phone = document.createElement("input");
-  phone.type="telephone";
+  phone.type="tel";
   phone.placeholder="+212*********";
   phone.id = "phone";
 
@@ -34,19 +34,67 @@ function renderSignUp(){
   const submitBtn= document.createElement("button");
   submitBtn.textContent ="S'inscrire";
 
-  submitBtn.addEventListener("click",()=>{
+
+  const fullNameRegex = /^[A-Za-zÀ-ÿ]+(?: [A-Za-zÀ-ÿ]+)+$/;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const phoneRegex = /^\+212 [67]\d{8}$/;
+  const passwordRegex = /^(?=.*[^A-Za-z0-9]).{8,}$/;
+
+
+submitBtn.addEventListener("click", () => {
+
+    const actualFullName = fullName.value;
     const actualEmail = email.value;
+    const actualPhone = phone.value;
     const actualPass = password.value;
-    console.log("email is :",actualEmail);
-    console.log("password is :",actualPass);
-  });
+    const actualPassVer = passwordVer.value;
+
+    console.log(fullNameRegex.test(actualFullName));
+    console.log(emailRegex.test(actualEmail));
+    console.log(phoneRegex.test(actualPhone));
+    console.log(passwordRegex.test(actualPass));
+
+    if (!fullNameRegex.test(actualFullName)) {
+        console.log("Nom invalide");
+        return;
+    }
+
+    if (!emailRegex.test(actualEmail)) {
+        console.log("Email invalide");
+        return;
+    }
+
+    if (!phoneRegex.test(actualPhone)) {
+        console.log("Téléphone invalide");
+        return;
+    }
+
+    if (!passwordRegex.test(actualPass)) {
+        console.log("Mot de passe invalide");
+        return;
+    }
+
+    if (actualPass !== actualPassVer) {
+        console.log("Les mots de passe ne correspondent pas");
+        return;
+    }
+
+    console.log("Toutes les données sont valides !");
+});
 
   root.appendChild(title);
+  root.appendChild(document.createElement("br"));
+  root.appendChild(document.createElement("br"));
   root.appendChild(fullName);
+  root.appendChild(document.createElement("br"));
   root.appendChild(email);
+  root.appendChild(document.createElement("br"));
   root.appendChild(phone);
+  root.appendChild(document.createElement("br"));
   root.appendChild(password);
+  root.appendChild(document.createElement("br"));
   root.appendChild(passwordVer);
+  root.appendChild(document.createElement("br"));
   root.appendChild(submitBtn);
 }
 
